3de4917a46f1f9b000e648c50cfb006f626be48b
Mohit Varma <mv501049@gmail.com>
2024-10-15 19:23:03 +0530

    Delete app/src/main/java/com/mohitvarma/jetpackcomposeappdesign/app5 directory

--- app/src/main/java/com/mohitvarma/jetpackcomposeappdesign/app5/models/attendance_reason/ReasonModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.compose.rally.app5.models.attendance_reason
-
-data class ReasonModel(
-    val AttendanceAbsentReasonsList: List<AttendanceAbsentReasons>,
-    val errorCode: Int,
-    val errorMessage: String
-)
